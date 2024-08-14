5d1d22000798fe2036962e3bb3546fe08ea39560
nitin <nitin@gmail.com>
2024-08-14 15:41:26 +0530

    dev1

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ instances = ec2.create_instances(
         ImageId="ami-0dafa01c8100180f8",
         MinCount=1,
         MaxCount=1,
-        InstanceType="t2-micro",
+        InstanceType="nitin",
         KeyName="KeyPair1"
     )
